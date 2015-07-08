c5acb7f19f00423dbd5961f9908282f4757fd83e
Roman Blanco <roman.blanco92@gmail.com>
2015-07-08 11:57:42 +0200

    Fixed threading

--- kaktus.py
+++ kaktus.py
@@ -3,16 +3,18 @@ import time
 import http.client
 import urllib.parse
 import urllib.request
+import socket
 import re
 import logging
 import json
 import threading
 import signal
 
-DEBUG = True
-LOADING_TIME = 10
-RECEIVING_TIME = 10
-TOKEN = ""
+DEBUG = False
+LOADING_TIME = 60*30
+RECEIVING_TIME = 2
+TOKEN = "*********************************************"
+
 
 class Application:
 
@@ -21,13 +23,11 @@ class Application:
         self.subscribers = Subscribers()
         self.article = Article()
         # start receiving thread
-        receiveThread = Receive(self.telegram)
-        # receiveThread.setName("receiveThread")
+        self.threading = threading.Thread(target=self.receivingThread)
         logging.debug('setting thread as daemon')
-        receiveThread.daemon = True
+        self.threading.daemon = True
         logging.debug('starting thread as daemon')
-        receiveThread.start()
-        # receiveThread.is_alive()
+        self.threading.start()
         # start main
         logging.debug('continue with main')
         self.main()
@@ -48,25 +48,48 @@ class Application:
                     self.telegram.sendMessage(int(subscriber), loadedArticle)
             time.sleep(LOADING_TIME)
 
+    def receivingThread(self):
+        logging.debug('starting receiving thread')
+        while True:
+            logging.debug('next iteration of receiving thread')
+            time.sleep(RECEIVING_TIME)
+            if self.telegram.checkOnline():
+                self.telegram.update()
+
     def interpret(self, response):
         """From response recognize which action should be done"""
         logging.debug('interpreting response: ' + str(response))
-        if response['message']['text'] == "/subscribe":
-            self.addSubscriber(response['message']['from']['id'])
-        elif response['message']['text'] == "/unsubscribe":
-            self.removeSubscriber(response['message']['from']['id'])
-        elif response['message']['text'] == "/help":
-            self.help(response['message']['from']['id'])
-        elif response['message']['text'] == "/last":
-            self.showLast(response['message']['from']['id'])
+        if 'text' in response['message'].keys():
+            if response['message']['text'] == "/subscribe":
+                self.addSubscriber(response['message']['from']['id'])
+            elif response['message']['text'] == "/unsubscribe":
+                self.removeSubscriber(response['message']['from']['id'])
+            elif response['message']['text'] == "/help":
+                self.help(response['message']['from']['id'])
+            elif response['message']['text'] == "/last":
+                self.showLast(response['message']['from']['id'])
 
     def addSubscriber(self, userId):
         """Add new subscriber to list"""
-        self.subscribers.add(userId)
+        if self.subscribers.add(userId):
+            self.telegram.sendMessage(
+                userId,
+                "You're now subscribing news from Kaktus operator")
+        else:
+            self.telegram.sendMessage(
+                userId,
+                "You are already subscribing news from Kaktus operator")
 
     def removeSubscriber(self, userId):
         """Remove subscriber from list"""
-        self.subscribers.remove(userId)
+        if self.subscribers.remove(userId):
+            self.telegram.sendMessage(
+                userId,
+                "You were removed from subscribing news from Kaktus operator")
+        else:
+            self.telegram.sendMessage(
+                userId,
+                "You weren't subscribing news from Kaktus operator")
 
     def showLast(self, userId):
         """Show newest article"""
@@ -88,7 +111,10 @@ class Connection:
     def loadSource():
         """Load source of page with news"""
         connection = http.client.HTTPSConnection("www.mujkaktus.cz")
-        connection.request("GET", "/novinky")
+        try:
+            connection.request("GET", "/novinky")
+        except socket.gaierror:
+            logging.error("nepodarilo se nacist data z webove stranky")
         response = connection.getresponse()
         content = response.read().decode('utf-8')
         return content
@@ -165,13 +191,19 @@ class Subscribers:
         """Add user's ID to list of subscribers"""
         if str(userId) not in self.subscribers:
             self.subscribers.append(str(userId))
-        logging.debug('new subscriber: ' + str(self.subscribers))
+            logging.debug('new subscriber: ' + str(self.subscribers))
+            return True
+        else:
+            return False
 
     def remove(self, userId):
         """Remove user's ID from list of subscribers"""
         if str(userId) in self.subscribers:
             self.subscribers.remove(str(userId))
-        logging.debug('one subscriber removed: ' + str(self.subscribers))
+            logging.debug('one subscriber removed: ' + str(self.subscribers))
+            return True
+        else:
+            return False
 
     def subscribersList(self):
         """Return list with all subscribers IDs"""
